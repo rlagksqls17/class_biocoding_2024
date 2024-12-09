a24a1e5ff076e12aca2495e8dcbd759cfc0bb2dd
rlagksqls17 <rlagksqls17@naver.com>
2024-12-09 15:25:08 +0900

    add code

---
+++ 7_파일_읽고_쓰기/train/train_2.py
@@ -0,0 +1,3 @@
+string = input("문장을 입력하세요 : ")
+search = input("검색할 단어를 입력하세요 : ")
+print(string.split(" ").count(search))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # biocoding_genome_class_python
+
+각 ppt에 적혀있는 경로 및 문제 번호를 따라 경로 접속하시면, 
+해당 문제의 예시 정답을 확인하실 수 있습니다.
